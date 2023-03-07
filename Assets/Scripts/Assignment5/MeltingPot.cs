f9f8e8184191fb75239999f077cc14f77cd276c1
Zhukova Darya <71393466+DashZi@users.noreply.github.com>
2023-03-07 20:22:44 +0100

    tried to add music, doors and interactions doesn't work(((((

--- Assets/Scripts/Assignment5/MeltingPot.cs
+++ Assets/Scripts/Assignment5/MeltingPot.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class MeltingPot : MonoBehaviour
 {
     public AudioSource AudioSource;
-    //public GameObject enemy;
     private bool activated = false;
+    
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Im the pot, the ingredient is inside");
@@ -15,6 +15,7 @@ public class MeltingPot : MonoBehaviour
             if (!activated)
             {
                 activated = true;
+                ScoreManager.instance.AddPoint();
                 AudioSource.enabled = true;
             }
         }
